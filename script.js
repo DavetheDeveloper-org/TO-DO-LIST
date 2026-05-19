4690d30a05b7c47fe9cb8603a6fb1d637a9e4f88
Dave <ogunsanyadavid08@gmail.com>
2026-05-19 11:28:04 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () =>{;
+const taskInput = document.getElementById('task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');
+const todosContainer = document.querySelector('todos-container')
+
+const toggleEmptyState = () => {
+    todosContainer.style.width = taskList.children.length > 0 ? '100%' : '50%'
+};
+
+const addTask= (event) => {
+    event.preventDefault();
+    const taskText= taskInput.value.trim();
+    if(!taskText){
+        return;
+    }
+    
+    const li = document.createElement('li')
+    li.innerHTML =`
+    <span>${taskText}</span>
+    <div class ="task-buttons">
+    <button class = "edit-btn"> <i class = "fa-solid fa-pen"></i></button>
+    <button class = "delete-btn"><i class = "fa-solid fa-trash"></i></button>
+    </div>
+    `
+    const editBtn = li.querySelector('.edit-btn');
+    editBtn.addEventListener('click', () => {
+        taskInput.value = li.querySelector('span').textContent;
+        li.remove()
+    })
+
+    li.querySelector('.delete-btn').addEventListener('click', () => {
+    li.remove()
+  })
+
+    taskList.appendChild(li);
+    if (taskInput.value = ''){
+        alert("OGA ENTER SOMETHING")
+    }
+};
+
+    addTaskBtn.addEventListener('click', addTask);
+});
